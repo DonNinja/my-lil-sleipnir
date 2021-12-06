6f9976f5bbd5a6305e40c5e4805843f565174437
DonNinja <yngvileo@gmail.com>
2021-12-06 13:12:40 +0000

    Tried making some kind of crashing thing, commented out in CollisionScript

--- My lil Sleipnir/Assets/Scripts/CollisionScript.cs
+++ My lil Sleipnir/Assets/Scripts/CollisionScript.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class CollisionScript : MonoBehaviour
 {
+    [SerializeField] GameObject horse;
+
     void OnCollisionEnter2D(Collision2D collision) {
         // TODO: Crash horse
         UnityEditor.EditorApplication.isPlaying = false;
+        //horse.GetComponent<Collider2D>().enabled = false;
+        //GetComponent<Collider2D>().enabled = false;
+        //Rigidbody2D horse_rigid = horse.GetComponent<Rigidbody2D>();
+        //horse_rigid.velocity = Random.insideUnitCircle.normalized * 20f;
+        //horse_rigid.mass = 0f;
+        //horse_rigid.rotation = Random.Range(1f, 180f);
+        //PlayerScript.instance.player_speed = 0;
+        //horse_rigid.gravityScale = 0f;
     }
 }
