ecd74dfb5e19cdecfec7f99dd243d45dfccb5de2
DonNinja <yngvileo@gmail.com>
2021-12-12 17:25:19 +0000

    Fixed Horse being weird on menu

--- My lil Sleipnir/Assets/AppleScript.cs
+++ My lil Sleipnir/Assets/AppleScript.cs
@@ -7,5 +7,7 @@ public class AppleScript : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision) {
         if (GameManager.instance)
             GameManager.instance.food_amount++;
+        Destroy(gameObject);
+        Destroy(this);
     }
 }
