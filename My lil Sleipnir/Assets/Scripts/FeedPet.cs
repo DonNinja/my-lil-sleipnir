7e5a11b7a30c8ba5fe1987294a8febd1f98398d3
DonNinja <yngvileo@gmail.com>
2021-12-12 13:52:05 +0000

    Added an animation when hav no appl

--- My lil Sleipnir/Assets/Scripts/FeedPet.cs
+++ My lil Sleipnir/Assets/Scripts/FeedPet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class FeedPet : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Animator anim;
 
     private Slider slider;
     private BarFill foodslider;
@@ -25,8 +25,9 @@ public class FeedPet : MonoBehaviour
             }
         }
         else {
-            Debug.Log("Not enough food to feed Sleipnir");
-            // Add func that highlights your coins showing the player you don't have enough
+            //Debug.Log("Not enough food to feed Sleipnir");
+            anim.ResetTrigger("Apples");
+            anim.SetTrigger("Apples");
         }
     }
     // Update is called once per frame
